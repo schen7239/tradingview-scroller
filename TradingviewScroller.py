d999bb2a782fbb2086da59fc1ec1070ac902f569
schen7239 <schen7239@gmail.com>
2023-07-30 18:22:15 -0400

    Allow user to choose between lazy and manual scrolling
    Add manual scrolling
    
    Completed V2

--- TradingviewScroller.py
+++ TradingviewScroller.py
@@ -1,5 +1,6 @@
 from classes.TradingViewWatcher import TradingViewWatcher
+from constants.TradingViewWatcherConstants import FileTypes, ScrollType
 
-watcher = TradingViewWatcher("./holdings/nasdaq_100.json", "JSON")
+watcher = TradingViewWatcher("./holdings/nasdaq_100.json", FileTypes.JSON, ScrollType.MANUAL)
 
-watcher.run(60)
+watcher.run(0)
--- classes/TradingViewWatcher.py
+++ classes/TradingViewWatcher.py
@@ -3,19 +3,44 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import json
 import time
+
 from .KeyboardHandler import KeyboardHandler
+from constants.TradingViewWatcherConstants import FileTypes, ScrollType
 
 class TradingViewWatcher:
-    def __init__(self, file, format = "JSON") -> None:
-        self.key_handler = KeyboardHandler()
-        if format == "JSON":
-            f = open(file)
-            self.stocks =  json.load(f)["stocks"]
+    def __init__(self, file, format = FileTypes.JSON, scroll_type = ScrollType.MANUAL) -> None:
+        self.scroll_type = scroll_type
+        match scroll_type:
+            case ScrollType.MANUAL:
+                self.key_handler = KeyboardHandler()
+            case ScrollType.LAZY:
+                self.key_handler = None
+            case default:
+                raise ValueError("INVALID SCROLL_TYPE")
+        match format:
+            case FileTypes.JSON:
+                f = open(file)
+                self.stocks =  json.load(f)["stocks"]
+            case default:
+                raise NotImplementedError("ONLY JSON WORKS")
     
     def login(self):
         pass
     
-    def run(self, position = 0):
+    def run_lazy_scrolling(self, position = 0):
+        browser = webdriver.Chrome()
+        
+        browser.get('https://www.tradingview.com/chart')
+        while True:
+            time.sleep(2)
+            browser.find_element(By.ID, "header-toolbar-symbol-search").click()
+            modal = browser.find_element(By.CLASS_NAME, "search-ZXzPWcCf")
+            time.sleep(0.5)
+            modal.send_keys(self.stocks[position])
+            modal.send_keys(Keys.RETURN)
+            position += 1
+    
+    def run_manual_scrolling(self, position = 0):
         list_size = len(self.stocks)
         browser = webdriver.Chrome()
         
@@ -45,3 +70,13 @@ class TradingViewWatcher:
             self.key_handler.set_last_key_pressed(None)
         self.key_handler.remove_listener()
         browser.quit()
+        
+    
+    def run(self, position = 0):
+        match self.scroll_type:
+            case ScrollType.LAZY:
+                self.run_lazy_scrolling(position)
+            case ScrollType.MANUAL:
+                self.run_manual_scrolling(position)
+            case default:
+                raise NotImplementedError("SOMETHING WRONG WITH CLASS. SHOULD BE CAUGHT EARLIER")
